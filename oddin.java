fc38147e75c14f1c1573afce0c1c51d59fe09666
BalajiStark <balajicse.2910@gmail.com>
2017-06-07 12:28:30 +0530

    Create oddin.java

---
+++ oddin.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class oddin {
+
+    public static void main(String[] args) {
+ JavaApplication13 j=new JavaApplication13();
+       Scanner in=new Scanner(System.in);
+       int n=in.nextInt(),r,i=0,flag=0;
+       int n1=in.nextInt();
+       String s=Integer.toString(n);
+      for(i=n+1;i<n1;i++)
+      {
+          if(i%2!=0)
+              System.out.print(i+" ");
+      }
+     }
+   }  
