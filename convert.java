54a61d640015b699accc4e1798dac751a6e0c6a2
BalajiStark <balajicse.2910@gmail.com>
2017-06-08 17:21:07 +0530

    Create convert.java

---
+++ convert.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class convert {
+
+    public static void main(String[] args) {
+
+       Scanner in=new Scanner(System.in);
+String s=in.nextLine();
+int x=Integer.parseInt(s);
+        System.out.println(x);
+    }
+}
