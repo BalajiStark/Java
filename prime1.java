9406312689288b20a9b65d7e047da50380b382fa
BalajiStark <balajicse.2910@gmail.com>
2017-06-09 17:36:10 +0530

    Update prime1.java

--- prime1.java
+++ prime1.java
@@ -1,10 +1,10 @@
 import java.util.*;
-public class prime1{
+public class JavaApplication13{
 
     public static void main(String[] args) {
 
        Scanner in=new Scanner(System.in);
- int n=in.nextInt(),i=0,flag=0;
+ int n=in.nextInt(),i=0,flag=0,count=0;
  for(i=1;i<n;i++)
  {   flag=0;
      for(int j=2;j<i;j++)
@@ -13,8 +13,9 @@ public class prime1{
              flag=1;
      }
      if(flag==0)
-         System.out.print(i+" ");
+         count++;
  } 
+        System.out.println(count);
      
     }
 }
