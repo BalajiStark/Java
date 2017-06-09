4c02018ee76ec5c29e8c9e7d125b8a011e2e9ed1
BalajiStark <balajicse.2910@gmail.com>
2017-06-09 17:34:43 +0530

    Create prime1.java

---
+++ prime1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class prime1{
+
+    public static void main(String[] args) {
+
+       Scanner in=new Scanner(System.in);
+ int n=in.nextInt(),i=0,flag=0;
+ for(i=1;i<n;i++)
+ {   flag=0;
+     for(int j=2;j<i;j++)
+     {
+         if(i%j==0)
+             flag=1;
+     }
+     if(flag==0)
+         System.out.print(i+" ");
+ } 
+     
+    }
+}
