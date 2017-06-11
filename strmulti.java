5d62b0a8064c70a4d08e42de214b5754945b6e38
BalajiStark <balajicse.2910@gmail.com>
2017-06-11 20:07:34 +0530

    Create strmulti.java

---
+++ strmulti.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class strmulti{
+
+    public static void main(String[] args) {
+
+       Scanner in=new Scanner(System.in);
+ String s=in.nextLine();
+ String s1=in.nextLine();
+ int x=Integer.parseInt(s);
+ int x1=Integer.parseInt(s1);
+ int ans=x*x1;
+ String res=Integer.toString(ans);
+        System.out.println(ans);
+ }
+    
+}
+
