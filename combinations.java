58a184ddf8102c385eb22dac1b7f34565e4ecb73
BalajiStark <balajicse.2910@gmail.com>
2017-06-11 19:59:24 +0530

    Create combinations.java

---
+++ combinations.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class combinations{
+
+    public static void main(String[] args) {
+
+       Scanner in=new Scanner(System.in);
+ int n=in.nextInt();
+ int k=in.nextInt();
+ for(int i=1;i<=n;i++)
+    {
+        for(int j=i+1;j<=n;j++)
+        {
+            System.out.println(i+""+j);        
+        }
+ 
+    }
+}
+    
+}
+
