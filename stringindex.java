5106c78c4013638effaaf3cd08073cfd94288932
BalajiStark <balajicse.2910@gmail.com>
2017-06-11 19:47:06 +0530

    Create stringindex.java

---
+++ stringindex.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class stringindex{
+
+    public static void main(String[] args) {
+
+       Scanner in=new Scanner(System.in);
+ String s=in.nextLine();
+ String s2=in.nextLine();
+ int n=s.indexOf(s2);
+        System.out.println(n);
+ 
+    }
+}
+
